d6ebcac3c3087b1660286f1de553d7bca26e86e9
aleenakrish <aleenakrish59@gmail.com>
2024-12-07 09:28:18 +0530

    mainproject

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,37 +1,89 @@
 import 'package:flutter/material.dart';
-import 'package:main_project/TaskScreen.dart';
 
-class HomeScreen extends StatefulWidget {
+import 'package:main_project/TaskScreen.dart';
+import 'package:main_project/appwriteservice.dart';
+
+class Homescreen extends StatefulWidget {
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  State<Homescreen> createState() => _HomescreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  List<Map<String, String>> tasks = []; // List to store tasks
+class _HomescreenState extends State<Homescreen> {
+  List<Map<String, dynamic>> tasks = [];
 
-  // Function to handle task deletion
-  void deleteTask(int index) {
-    setState(() {
-      tasks.removeAt(index);
-    });
+  @override
+  void initState() {
+    super.initState();
+    fetchTasks();
   }
 
-  // Function to handle task editing
-  Future<void> editTask(int index) async {
+  
+  Future<void> fetchTasks() async {
+    try {
+      final response = await appwriteService.database.listDocuments(
+        databaseId: '6753b00b0025f5bbcb65', 
+        collectionId: '6753b02e00365ac518fd', 
+      );
+
+      setState(() {
+        tasks = response.documents
+            .map((doc) => {
+                  'id': doc.$id,
+                  'title': doc.data['title'],
+                  'description': doc.data['description'],
+                  'date': doc.data['date'],
+                  'time': doc.data['time']
+                })
+            .toList();
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to fetch tasks: $e')),
+      );
+    }
+  }
+
+  
+  void editTask(String taskId, String title, String description, String date,
+      String time) async {
     final result = await Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => Taskscreen(
-          title: tasks[index]['title']!,
-          description: tasks[index]['description']!,
+        builder: (context) => TaskScreen(
+          taskId: taskId,
+          initialTitle: title,
+          initialDescription: description,
+          initialDate: date,
+          initalTime: time,
         ),
       ),
     );
 
-    if (result != null && result is Map<String, String>) {
+    if (result != null) {
+      fetchTasks(); 
+    }
+  }
+
+ 
+  Future<void> deleteTask(String taskId) async {
+    try {
+      await appwriteService.database.deleteDocument(
+        databaseId: '6753b00b0025f5bbcb65', 
+        collectionId: '6753b02e00365ac518fd', 
+        documentId: taskId,
+      );
+
       setState(() {
-        tasks[index] = result; // Update the task with the new values
+        tasks.removeWhere((task) => task['id'] == taskId);
       });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Task deleted successfully')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to delete task: $e')),
+      );
     }
   }
 
@@ -39,97 +91,115 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 106, 241, 106),
-        title: Text(
-          'Task',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-          ),
+        backgroundColor: Colors.green,
+        title: const Text(
+          'Echo Note',
+          style: TextStyle(color: Colors.white),
         ),
-        centerTitle: true,
+        centerTitle: false,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: tasks.isEmpty
-            ? Center(
-                child: Text(
-                  'New tasks added',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                ),
-              )
-            : GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  childAspectRatio: 2,
-                ),
-                itemCount: tasks.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Colors.lightGreen,
-                    ),
-                    padding: EdgeInsets.all(16),
+      body: tasks.isEmpty
+          ? const Center(
+              child: Text(
+                'Add new Task',
+                style: TextStyle(fontSize: 18, color: Colors.grey),
+              ),
+            )
+          : GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, 
+                crossAxisSpacing: 10.0, 
+                mainAxisSpacing: 10.0, 
+                childAspectRatio: 0.90, 
+              ),
+              padding: const EdgeInsets.all(10.0), 
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                final task = tasks[index];
+                return Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  elevation: 3.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          tasks[index]['title']!,
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
+                          task['title'],
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
                         ),
-                        SizedBox(height: 8),
-                        Text(
-                          tasks[index]['description']!,
-                          style: TextStyle(fontSize: 14, color: Colors.white70),
+                        const SizedBox(height: 5),
+                        Expanded(
+                          child: Text(
+                            task['description'],
+                            style: const TextStyle(color: Colors.grey),
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                          ),
                         ),
-                        Spacer(),
+                        Row(
+                          children: [
+                            Text(
+                              task['date'],
+                              style: const TextStyle(
+                                  fontSize: 12, color: Colors.black54),
+                            ),
+                            const Spacer(), 
+                            Text(
+                              task['time'],
+                              style: const TextStyle(
+                                  fontSize: 12, color: Colors.black54),
+                            ),
+                          ],
+                        ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.end,
                           children: [
                             IconButton(
-                              icon: Icon(Icons.edit, color: Colors.white),
-                              onPressed: () => editTask(index),
+                              icon: const Icon(Icons.edit, color: Colors.blue),
+                              onPressed: () => editTask(
+                                  task['id'],
+                                  task['title'],
+                                  task['description'],
+                                  task['date'],
+                                  task['time']),
                             ),
                             IconButton(
-                              icon: Icon(Icons.delete, color: Colors.white),
-                              onPressed: () => deleteTask(index),
+                              icon: const Icon(Icons.delete, color: Colors.red),
+                              onPressed: () => deleteTask(task['id']),
                             ),
                           ],
                         ),
                       ],
                     ),
-                  );
-                },
-              ),
-      ),
+                  ),
+                );
+              },
+            ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final result = await Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => Taskscreen()),
-          );
-
-          if (result != null && result is Map<String, String>) {
-            setState(() {
-              tasks.add(result);
-            });
-          }
+        onPressed: () {
+          navigateToTaskScreen();
         },
-        backgroundColor: Colors.lightGreen,
-        child: Icon(
-          Icons.task,
-          size: 30,
-          color: Colors.white,
-        ),
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.add),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
+  }
+
+ 
+  void navigateToTaskScreen() async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => TaskScreen()),
+    );
+
+    if (result != null) {
+      fetchTasks();
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:main_project/HomeScreen.dart';
-import 'package:main_project/TaskScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: Homescreen(),
     );
   }
 }
